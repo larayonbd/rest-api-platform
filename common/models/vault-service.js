64ebc91bcbc7c01c84d353971329307271c1aa4b
Luis Rayon <larayon@datiobd.com>
2018-07-18 12:09:08 -0500

    Fix unseal method

--- common/models/vault-service.js
+++ common/models/vault-service.js
@@ -1,6 +1,7 @@
 'use strict';
 var async = require('async');
 var axios = require('axios');
+var exec = require('child_process').exec;
 
 module.exports = function(Vaultservice) {
 
@@ -27,21 +28,22 @@ module.exports = function(Vaultservice) {
                 function(step1, callback) {
 
                     var baseUrl = vaultUrl + '/v1/sys/seal';
-                    axios({
-                            method: 'PUT',
-                            url: baseUrl,
-                            headers: {
-                                'X-Vault-Token': Key
-                            }
-                        })
-                        .then(function(response) {
-                            responseService.sealed = true;
+                    var child;
+                    var url = "curl -s -o /dev/null -w '%{http_code}' --header X-Vault-Token:" + Key + " -X PUT " + baseUrl;
+                    child = exec(url, function(error, stdout, stderr) {
+                        if (stdout) {
+                            responseService.sealed = (stdout = 204) ? true : false;
                             callback(null, "ok")
-                        })
-                        .catch(function(error) {
-                            responseService = error.response.data;
-                            callback(null, "ok");
-                        });
+                        } else {
+                            responseService.data = "Connection refused";
+                            callback(null, "ok")
+
+
+                        }
+
+
+                    });
+
                 }
             ],
             function(err, response) {
@@ -51,16 +53,23 @@ module.exports = function(Vaultservice) {
     };
 
 
-    Vaultservice.unseal = function(key1, key2, key3, cb) {
+    Vaultservice.unseal = function(data, cb) {
         var vaultUrl = "127.0.0.1:8200";
-        var responseService = [];
+        var responseService = {
+            "status": "run",
+            "key1": "undefined",
+            "key2": "undefined",
+            "key3": "undefined"
+        };
         var unsealKeys = [{
-            "key": key1
+            "key": data.key1
         }, {
-            "key": key2
+            "key": data.key2
         }, {
-            "key": key3
+            "key": data.key3
         }];
+        var baseUrl = vaultUrl + '/v1/sys/unseal';
+        var child;
         async.waterfall(
             [
                 function(callback) {
@@ -79,33 +88,62 @@ module.exports = function(Vaultservice) {
                     });
                 },
                 function(step1, callback) {
-                    var baseUrl = vaultUrl + '/v1/sys/unseal';
-                    for (var i = 0; i < 3; i++) {
-                        var jsonData = JSON.parse(JSON.stringify(unsealKeys[i]));
-                        axios({
-                                method: 'put',
-                                url: baseUrl,
-                                data: jsonData
-                            })
-                            .then(function(response) {
-                                responseService.push(response.data);
-                                if (i = 2) {
-                                    callback(null, "ok")
-                                }
-                            })
-                            .catch(function(error) {
-                                responseService.push(error.response.data);
-                                if (i = 2) {
-                                    callback(null, "ok")
-                                }
-                            });
+                    const jsonData = JSON.stringify(unsealKeys[0]);
+                    var url = "curl -s  -o /dev/null -w '%{http_code}'  --header 'Content-Type: application/json' -X PUT -d '" + `${jsonData}` + "' " + baseUrl;
+                    child = exec(url, function(error, stdout, stderr) {
+                        if (stdout) {
+                            if (stdout == 200) {
+                                responseService.key1 = true;
+                                callback(null, "ok");
+                            } else {
+                                responseService.key1 = false;
+                                callback(null, "ok");
 
+                            }
+                        } else {
+                            callback(null, "ok");
+                        }
+                    });
+                },
+                function(step2, callback) {
+                    const jsonData = JSON.stringify(unsealKeys[1]);
+                    var url = "curl -s  -o /dev/null -w '%{http_code}'  --header 'Content-Type: application/json' -X PUT -d '" + `${jsonData}` + "' " + baseUrl;
+                    child = exec(url, function(error, stdout, stderr) {
+                        if (stdout) {
+                            if (stdout == 200) {
+                                responseService.key2 = true;
+                                callback(null, "ok");
+                            } else {
+                                responseService.key2 = false;
+                                callback(null, "ok");
 
-                    }
+                            }
+                        } else {
+                            callback(null, "ok");
+                        }
+                    });
+                },
+                function(step3, callback) {
+                    const jsonData = JSON.stringify(unsealKeys[2]);
+                    var url = "curl -s  -o /dev/null -w '%{http_code}'  --header 'Content-Type: application/json' -X PUT -d '" + `${jsonData}` + "' " + baseUrl;
+                    child = exec(url, function(error, stdout, stderr) {
+                        if (stdout) {
+                            if (stdout == 200) {
+                                responseService.key3 = true;
+                                callback(null, "ok");
+                            } else {
+                                responseService.key3 = false;
+                                callback(null, "ok");
 
+                            }
+                        } else {
+                            callback(null, "ok");
+                        }
+                    });
                 }
             ],
             function(err, response) {
+
                 return cb(null, responseService);
             }
         );
@@ -134,18 +172,21 @@ module.exports = function(Vaultservice) {
                 function(step1, callback) {
 
                     var baseUrl = vaultUrl + '/v1/sys/health';
-                    axios({
-                            method: 'get',
-                            url: baseUrl
-                        })
-                        .then(function(response) {
-                            responseService = response.data;
+                    var child;
+                    var url = "curl -k " + baseUrl;
+                    child = exec(url, function(error, stdout, stderr) {
+                        if (stdout) {
+                            responseService = JSON.parse(stdout);
                             callback(null, "ok")
-                        })
-                        .catch(function(error) {
-                            responseService = error.response.data;
-                            callback(null, "ok");
-                        });
+                        } else {
+                            responseService.status = "Connection refused";
+                            callback(null, "ok")
+
+
+                        }
+
+
+                    });
                 }
             ],
             function(err, response) {
@@ -172,22 +213,16 @@ module.exports = function(Vaultservice) {
                 path: '/unseal',
                 verb: 'put'
             },
-            accepts: [{
-                    arg: 'key1',
-                    type: 'string'
-                },
-                {
-                    arg: 'key2',
-                    type: 'string'
-                },
-                {
-                    arg: 'key3',
-                    type: 'string'
+            accepts: {
+                arg: 'data',
+                type: 'object',
+                http: {
+                    source: 'body'
                 }
-            ],
+            },
             returns: {
                 arg: 'data',
-                type: 'array'
+                type: 'object'
             }
         }
     );
